ae9f07ad7a1f55818efcc81667a87781e1bda3ae
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-11 10:11:29 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,46 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    "#{params[:name].reverse}"
+  end
+
+  get '/square/:number' do 
+    "#{self.square(params[:number])}"
+  end
+
+  def square(number)
+    number.to_i ** 2
+  end
+
+  get '/say/:number/:phrase' do 
+    "#{params[:phrase] * params[:number].to_i}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    output_array = []
+    params.each{|key, value|
+      output_array.push(value)
+    }
+    "#{output_array.join(' ')}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+    case params[:operation]
+    when 'add'
+      "#{number1 + number2}"
+    when 'subtract'
+      "#{number1 - number2}"
+    when 'multiply'
+      "#{number1 * number2}"
+    when 'divide'
+      "#{number1 / number2}"
+    else
+      "unsupported"
+    end
+  end
+
+
 end
